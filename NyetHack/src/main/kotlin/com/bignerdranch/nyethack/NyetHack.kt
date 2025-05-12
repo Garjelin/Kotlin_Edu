8acc76fe9fd1f4a17a6b034ab1b8b90c8b8b8344
Garjelin <sergeyyakimov89@gmail.com>
2025-05-13 00:03:20 +0500

    2025.05.12_2

---
+++ NyetHack/src/main/kotlin/com/bignerdranch/nyethack/NyetHack.kt
@@ -0,0 +1,24 @@
+package org.example.com.bignerdranch.nyethack
+
+val player = Player("Jason", "Jacksonville", 100, false)
+
+fun main() {
+//    changeNarrationMood()
+    val mortality = if (player.isImmortal) "an immortal" else "a mortal"
+    narrate("${player.name}, ${player.title}, heads to the town square")
+    visitTavern()
+    player.castFireball()
+}
+
+private fun makeYellow(message: String): String = "\u001b[33;1m$message\u001b[0m"
+
+private fun promptHeroName(): String {
+    narrate("A hero enters the town of Kronstadt. What is their name?", ::makeYellow)
+//    val input = readLine()
+//    require(heroName != null && heroName.isNotEmpty()) {
+//        "The hero must have a name."
+//    }
+//    return input
+    println("Madrigal")
+    return "Madrigal"
+}
---
+++ NyetHack/src/main/kotlin/com/bignerdranch/nyethack/Player.kt
@@ -0,0 +1,34 @@
+package org.example.com.bignerdranch.nyethack
+
+class Player(
+    initialName: String,
+    hometown: String,
+    healthPoints: Int,
+    isImmortal: Boolean
+) {
+    var name = initialName
+        get() = field.replaceFirstChar { it.uppercase() }
+        private set(value) {
+            field = value.trim()
+        }
+    val hometown = hometown
+    var healthPoints = healthPoints
+    val isImmortal = isImmortal
+
+    val title: String
+        get() = when {
+            name.all { it.isDigit() } -> "The Identifiable"
+            name.none { it.isLetter() } -> "The Witness Protection Member"
+            name.count { it.lowercase() in "aeiou" } > 4 -> "The Master of Vowels"
+            else -> "The Renowned Hero"
+        }
+
+    fun castFireball(numFireballs: Int = 2) {
+        narrate("A glass of Fireball springs into existence (x$numFireballs)")
+    }
+
+    fun changeName(newName: String) {
+        narrate("$name legally changes their name to $newName")
+        name = newName
+    }
+}
