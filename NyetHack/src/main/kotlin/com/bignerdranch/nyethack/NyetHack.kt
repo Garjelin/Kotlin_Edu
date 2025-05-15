c7e68ffc3e2966dbf2d25da74398dc1e21f9eef5
Sergey Yakimov <yakimovsa@tech.bcs.ru>
2025-05-15 18:27:01 +0500

    2025-05-15_1

--- NyetHack/src/main/kotlin/com/bignerdranch/nyethack/NyetHack.kt
+++ NyetHack/src/main/kotlin/com/bignerdranch/nyethack/NyetHack.kt
@@ -1,13 +1,18 @@
 package org.example.com.bignerdranch.nyethack
 
-val player = Player("Estragon")
+lateinit var player: Player
 
 fun main() {
+    narrate("Welcome to NyetHack!")
+    val playerName = promptHeroName()
+    player = Player(playerName)
 //    changeNarrationMood()
+    player.prophesize()
     val mortality = if (player.isImmortal) "an immortal" else "a mortal"
-    narrate("${player.name}, ${player.title}, heads to the town square")
+    narrate("${player.name} of ${player.hometown}, ${player.title}, heads to the town square")
     visitTavern()
     player.castFireball()
+    player.prophesize()
 }
 
 private fun makeYellow(message: String): String = "\u001b[33;1m$message\u001b[0m"
