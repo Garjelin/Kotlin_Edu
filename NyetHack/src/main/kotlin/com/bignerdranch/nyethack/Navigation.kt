ebf0dde092571435d8ecc527b78b3ec12fbe4bf2
Garjelin <sergeyyakimov89@gmail.com>
2025-05-21 07:14:54 +0500

    2025.05.20_1

--- NyetHack/src/main/kotlin/com/bignerdranch/nyethack/Navigation.kt
+++ NyetHack/src/main/kotlin/com/bignerdranch/nyethack/Navigation.kt
@@ -1,3 +1,12 @@
 package com.bignerdranch.nyethack
 
 data class Coordinate(val x: Int, val y: Int)
+
+enum class Direction(
+    private val directionCoordinate: Coordinate
+) {
+    North(Coordinate(0, -1)),
+    East(Coordinate(1, 0)),
+    South(Coordinate(0, 1)),
+    West(Coordinate(-1, 0))
+}
